de42a8e59d479f4e763a580f13eccbcaef2ae98b
qkrdydpyc <42770240+qkrdydpyc@users.noreply.github.com>
2020-08-31 13:48:08 +0900

    Add files via upload
    요일체크하는 코드 일수를 합치고 7로 나눠 1월 1일이 금요일로 시작하기 때문에 금토일월화수목으로 배열을 설정, 12월의 일수를 설정 후 반복문을 통해 합친다. 
    동적할당 확인 필요

---
+++ 소스.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int a, int b) {
+    int month[12] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
+    char* day[] = { "FRI","SAT","SUN","MON","TUE","WED","THU" };
+    int c = 0;
+    char* answer = (char*)malloc(sizeof(day));
+    for (int i = 0; i < a-1; i++)
+    {
+        c += month[i];
+    }
+    c = c + b-1;
+    /*
+    1금2토3일4월5화6수7목8금
+    */
+    int tmp;
+    tmp = c % 7;
+    answer = day[tmp];
+    printf("\n%s\n", answer);
+    return answer;
+    free(answer);
+}
+
+void main() {
+    solution(5, 24);
+}
